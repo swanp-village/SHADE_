4e71072d32c77e7e078d8e612387cf52380445c3
swanp-village <92138575+swanp-village@users.noreply.github.com>
2026-08-03 11:06:50 +0900

    Update SHADE.py

--- MRR/SHADE.py
+++ MRR/SHADE.py
@@ -261,9 +261,10 @@ def SHADE(func, bounds, params, pop_size, max_iter, H,  tol, callback=None, rng=
 
 
         mut_cross_paras = [[MF_para_H[r[j]], MCR_para_H[r[j]], bounds, j, pop_size, obj_list_G, populations_G, P_i_int, Archive, xdim, np.random.randint(0, 2 ** 32 -1)] for j in range(pop_size)]
-        p = Pool(processes = pop_size)
+        #p = Pool(processes = pop_size)
 
-        tmp = list( p.map(wrapper_mut_cross, mut_cross_paras) ) #一時的な答え、この後スライスし、必要なところだけ切り取る
+        #tmp = list( p.map(wrapper_mut_cross, mut_cross_paras) ) #一時的な答え、この後スライスし、必要なところだけ切り取る
+        tmp = [wrapper_mut_cross(args) for args in mut_cross_paras]
         if i < 2:
             print(f"[TRACE-09] gen={i} Pool.map done, tmp_len={len(tmp)}")
             for I, t in enumerate(tmp):
