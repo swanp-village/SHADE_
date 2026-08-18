cef320196b5693290d421d097bd1bb186a0836ed
swanp-village <92138575+swanp-village@users.noreply.github.com>
2026-08-18 13:30:31 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -298,7 +298,7 @@ def optimize_K(             #通常のSHADE用
                    pop_size=17, 
                    max_iter = 500,
                    H = 100,
-                   #tol = 0.0001, 
+                   tol = 0.0001, 
                    callback = None, 
                    rng = None
                   )
